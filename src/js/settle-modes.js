19d14c8badbe27744c3623a97d89d26aba7d1594
ViniciusLCLima <87040226+ViniciusLCLima@users.noreply.github.com>
2024-06-14 17:23:10 -0300

    Making mode button moon icon change on mode change

--- src/js/settle-modes.js
+++ src/js/settle-modes.js
@@ -1,29 +1,38 @@
 class Mode{
-    constructor(elementsColor, backgroundColor, textColor, inputColor, iconFirstClass){
-        this.elementsColor = elementsColor
-        this.backgroundColor = backgroundColor
-        this.textColor = textColor
-        this.inputColor = inputColor
-        this.iconFirstClass = iconFirstClass
+    constructor(elementsColor, backgroundColor, textColor, inputColor, nightIconFAStyleClass){
+        this.cssVars = {}
+        this.cssVars.elementsColor = elementsColor
+        this.cssVars.backgroundColor = backgroundColor
+        this.cssVars.textColor = textColor
+        this.cssVars.inputColor = inputColor
+        this.nightIconFAStyleClass = nightIconFAStyleClass
     }
 }
 
 const modeBtn = document.getElementById('modeBtn')
 const nightIcon = modeBtn.firstChild
 
-const lightMode = new Mode('hsl(0, 0%, 100%)', 'hsl(0, 0%, 98%)', 'hsl(200, 15%, 8%)', 'hsl(0, 0%, 52%)')
-const darkMode = new Mode('hsl(209, 23%, 22%)', 'hsl(207, 26%, 17%)', 'hsl(0, 0%, 100%)', 'hsl(0, 0%, 52%)')
+const lightMode = new Mode('hsl(0, 0%, 100%)', 'hsl(0, 0%, 98%)', 'hsl(200, 15%, 8%)', 'hsl(0, 0%, 52%)', 'fa-regular')
+const darkMode = new Mode('hsl(209, 23%, 22%)', 'hsl(207, 26%, 17%)', 'hsl(0, 0%, 100%)', 'hsl(0, 0%, 52%)', 'fa-solid')
 const modes = [lightMode, darkMode]
 
 const handleModeBtnClick = () =>{
     Window.vLCountriesAPI.darkModeOn = !Window.vLCountriesAPI.darkModeOn
-    const modeIdx = (Window.vLCountriesAPI.darkModeOn)? 1:0
-    const mode = modes[modeIdx]
-    for (const prop of Object.keys(mode)){
-        document.documentElement.style.setProperty(`--${prop}`, mode[prop])
+    let modeIdx
+    let disabledModeNightIconFAStyleClass
+    if (Window.vLCountriesAPI.darkModeOn){
+        modeIdx = 1
+        disabledModeNightIconFAStyleClass = modes[0].nightIconFAStyleClass
+    } else {
+        modeIdx = 0
+        disabledModeNightIconFAStyleClass = modes[1].nightIconFAStyleClass
     }
+    const mode = modes[modeIdx]
+    for (const cssVar of Object.keys(mode.cssVars)){
+        document.documentElement.style.setProperty(`--${cssVar}`, mode.cssVars[cssVar])
+    }
+    if (!nightIcon.classList.replace(disabledModeNightIconFAStyleClass, mode.nightIconFAStyleClass)) nightIcon.classList.add(mode.nightIconFAStyleClass)
     window.localStorage.setItem('darkModeOn', Window.vLCountriesAPI.darkModeOn)
-    
 }
 
 const setModesBtnClickHandler = ()=>{
