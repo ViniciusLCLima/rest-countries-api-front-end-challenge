ddced59291c9fbdee64985f395eb8a8264b2acbd
ViniciusLCLima <87040226+ViniciusLCLima@users.noreply.github.com>
2024-06-17 18:07:08 -0300

    Make dark mode button look more clickable on hover Adds special styles on hover and increases width and padding of button.

--- src/js/settle-modes.js
+++ src/js/settle-modes.js
@@ -1,7 +1,8 @@
 class Mode{
-    constructor(elementsColor, backgroundColor, textColor, inputColor, nightIconFAStyleClass){
+    constructor(elementsColor, elementsColorTint, backgroundColor, textColor, inputColor, nightIconFAStyleClass){
         this.cssVars = {}
         this.cssVars.elementsColor = elementsColor
+        this.cssVars.elementsColorTint = elementsColorTint
         this.cssVars.backgroundColor = backgroundColor
         this.cssVars.textColor = textColor
         this.cssVars.inputColor = inputColor
@@ -12,8 +13,8 @@ class Mode{
 const modeBtn = document.getElementById('modeBtn')
 const nightIcon = modeBtn.firstChild
 
-const lightMode = new Mode('hsl(0, 0%, 100%)', 'hsl(0, 0%, 98%)', 'hsl(200, 15%, 8%)', 'hsl(0, 0%, 52%)', 'fa-regular')
-const darkMode = new Mode('hsl(209, 23%, 22%)', 'hsl(207, 26%, 17%)', 'hsl(0, 0%, 100%)', 'hsl(0, 0%, 52%)', 'fa-solid')
+const lightMode = new Mode('hsl(0, 0%, 100%)', 'hsl(0, 0%, 96%)', 'hsl(0, 0%, 98%)', 'hsl(200, 15%, 8%)', 'hsl(0, 0%, 52%)', 'fa-regular')
+const darkMode = new Mode('hsl(209, 23%, 22%)', 'hsl(209, 23%, 28%)', 'hsl(207, 26%, 17%)', 'hsl(0, 0%, 100%)', 'hsl(0, 0%, 52%)', 'fa-solid')
 const modes = [lightMode, darkMode]
 
 const handleModeBtnClick = () =>{
