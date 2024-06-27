4f2e9911d92efe5ca1bdd2411ed3e6ccfc6d50a1
ViniciusLCLima <87040226+ViniciusLCLima@users.noreply.github.com>
2024-06-27 14:16:56 -0300

    Give less highlight to search bar's placeholder Also to get closer to proposed design, decreased size and changed color

--- src/js/settle-modes.js
+++ src/js/settle-modes.js
@@ -1,20 +1,22 @@
 class Mode{
-    constructor(elementsColor, elementsColorTint, backgroundColor, textColor, inputColor, nightIconFAStyleClass){
+    constructor(elementsColor, elementsColorTint, backgroundColor, textColor, inputColor, placeholderColor, nightIconFAStyleClass){
         this.cssVars = {}
         this.cssVars.elementsColor = elementsColor
         this.cssVars.elementsColorTint = elementsColorTint
         this.cssVars.backgroundColor = backgroundColor
         this.cssVars.textColor = textColor
         this.cssVars.inputColor = inputColor
+        this.cssVars.placeholderColor = placeholderColor
         this.nightIconFAStyleClass = nightIconFAStyleClass
     }
 }
 
 const modeBtn = document.getElementById('modeBtn')
 const nightIcon = modeBtn.firstChild
+const WHITE_STR = 'hsl(0, 0%, 100%)'
 
-const lightMode = new Mode('hsl(0, 0%, 100%)', 'hsl(0, 0%, 96%)', 'hsl(0, 0%, 98%)', 'hsl(200, 15%, 8%)', 'hsl(0, 0%, 52%)', 'fa-regular')
-const darkMode = new Mode('hsl(209, 23%, 22%)', 'hsl(209, 23%, 28%)', 'hsl(207, 26%, 17%)', 'hsl(0, 0%, 100%)', 'hsl(0, 0%, 52%)', 'fa-solid')
+const lightMode = new Mode(WHITE_STR, 'hsl(0, 0%, 96%)', 'hsl(0, 0%, 98%)', 'hsl(200, 15%, 8%)', 'hsl(0, 0%, 52%)', 'hsl(0, 0%, 75%)', 'fa-regular')
+const darkMode = new Mode('hsl(209, 23%, 22%)', 'hsl(209, 23%, 28%)', 'hsl(207, 26%, 17%)', WHITE_STR, 'hsl(0, 0%, 52%)', WHITE_STR, 'fa-solid')
 const modes = [lightMode, darkMode]
 
 const handleModeBtnClick = () =>{
